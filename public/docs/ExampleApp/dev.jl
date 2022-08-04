590b93c1e4d49bc2a57763f04548d5a384649cc8
emmy <52672675+emmettgb@users.noreply.github.com>
2022-08-04 01:34:40 -0500

    this was not meant to be evaluated

--- public/docs/ExampleApp/dev.jl
+++
@@ -1,8 +0,0 @@
-using Pkg; Pkg.activate(".")
-using Toolips
-using Revise
-using ExampleApp
-
-IP = "127.0.0.1"
-PORT = 8000
-ExampleAppServer = ExampleApp.start(IP, PORT)
--- public/docs/ExampleApp/prod.jl
+++
@@ -1,7 +0,0 @@
-using Pkg; Pkg.activate(".")
-using Toolips
-using ExampleApp
-
-IP = "127.0.0.1"
-PORT = 8000
-ExampleAppServer = ExampleApp.start(IP, PORT)
